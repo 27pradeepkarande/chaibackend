e9fdab0eab0aa4d12201e409563d17c5b19f5a2f
Pradeep Karande <pradeepkarande27@gmail.com>
2024-03-13 16:03:33 +0530

    Chai or Backend

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log("Chai or Code");
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) =>{
+    res.send("Pradeepkarande27")
+})
+
+app.get('/login', (req,res) =>{
+    res.send("<h1> Please Login in Chai or Code </h1>")
+})
+
+app.get('/youtube', (req, res) =>{
+    res.send("<h2> Chai or Code </h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
